478e8e0ac3d6b0b31f65c1bf7f870234e6be8c73
saucey-lackey7 <georgee.vellukunnel@gmail.com>
2026-01-15 16:20:24 +0100

    Lab 2 Version 2.0

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-@app.get("/{surface_area}")
-def read_item(surface_area: int, name: str, price: float):
+def validate_item(surface_area: int, price: float):
     if surface_area < 0:
         raise HTTPException(status_code=400, detail="Surface area cannot be negative")
     if price < 0:
         raise HTTPException(status_code=400, detail="Price cannot be negative")
+
+@app.get("/{surface_area}")
+def read_item(surface_area: int, name: str, price: float):
+    validate_item(surface_area, price)
     
     return {
         "surface_area": surface_area,
         "name": name,
         "price_euros": price
     }
+
